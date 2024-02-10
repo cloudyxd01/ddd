b7717cedcfd56d6d0924b27c3e91d13a25c2f7cc
cloudyxd01 <157258929+cloudyxd01@users.noreply.github.com>
2024-02-10 19:54:35 +0700

    Add files via upload

---
+++ flushdns.bat
@@ -0,0 +1,14 @@
+@echo off
+set/p a=To Apply Click Enter To Continue)
+echo You have chosen the mode %a%
+echo Building configuration... Please wait...
+ipconfig /release
+echo Building configuration... Please wait...
+ipconfig /renew
+echo Building configuration... Please wait...
+ipconfig /flushdns
+echo 100% 
+netstat -e
+echo Successfully : Press Any Key To Continue)
+set/p z=
+pause>NUL
