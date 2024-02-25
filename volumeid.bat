6c330762244c278a288d966317e4a686f323abee
cloudyxd01 <157258929+cloudyxd01@users.noreply.github.com>
2024-02-25 20:23:20 +0700

    Add files via upload

---
+++ volumeid.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Spoofing System Information...
+cd %~dp0
+::Volumeid64.exe C: "%random%-%random%"
+Volumeid64.exe C: "%random%-%random%"
+cls
+echo Successfully Spoofed. Press any key to return to the main menu...
+
+
+
+
