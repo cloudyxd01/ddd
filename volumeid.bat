558f07e5c73dd8eea04ea5dc6eb65658616a0142
cloudyxd01 <157258929+cloudyxd01@users.noreply.github.com>
2024-02-26 00:12:13 +0700

    Update volumeid.bat

--- volumeid.bat
+++ volumeid.bat
@@ -3,6 +3,9 @@ echo Spoofing System Information...
 cd %~dp0
 ::Volumeid64.exe C: "%random%-%random%"
 Volumeid64.exe C: "%random%-%random%"
+Volumeid64.exe D: "%random%-%random%"
+Volumeid64.exe E: "%random%-%random%"
+Volumeid64.exe F: "%random%-%random%"
 echo Successfully Spoofed. Press any key to return to the main menu...
 
 
