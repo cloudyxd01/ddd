cce8fd551ba115ea520ad2ddbf1235d5ccd47d87
cloudyxd01 <157258929+cloudyxd01@users.noreply.github.com>
2024-02-27 15:59:11 +0700

    Add files via upload

--- volumeid.bat
+++ volumeid.bat
@@ -1,10 +1,11 @@
+
 @echo off
 echo Spoofing System Information...
-Volumeid.exe C: "%random%-%random%"
-Volumeid.exe D: "%random%-%random%"
-Volumeid.exe E: "%random%-%random%"
-Volumeid.exe F: "%random%-%random%"
-pause
+Volumeid64.exe C: "%random%-%random%"
+Volumeid64.exe D: "%random%-%random%"
+Volumeid64.exe E: "%random%-%random%"
+Volumeid64.exe F: "%random%-%random%"
+pause > nul
 echo Successfully Spoofed. Press any key to return to the main menu...
 
 
