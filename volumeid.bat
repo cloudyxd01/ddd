891ad059975afaafea32d9194dad370f6bb665c7
cloudyxd01 <157258929+cloudyxd01@users.noreply.github.com>
2024-02-28 14:52:25 +0700

    Update volumeid.bat

--- volumeid.bat
+++ volumeid.bat
@@ -5,5 +5,5 @@ Volumeid64.exe C: "1559-%random%"
 Volumeid64.exe D: "1559-%random%"
 Volumeid64.exe E: "1559-%random%"
 Volumeid64.exe F: "1559-%random%"
-pause > nul
+timeout 1
 echo Successfully Spoofed. Press any key to return to the main menu...
